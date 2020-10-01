3a63323d93b5a390fc28bbd7736ec31d01bb2abe
Yanyan Sofiyan <yanyansofiyan89@outlook.com>
2020-10-01 22:34:12 +0700

    invalid syntax
    silahkan diperbaiki

--- A2.1900018/pratikum 2/Main.py
+++ A2.1900018/pratikum 2/Main.py
@@ -7,15 +7,15 @@ hero2 = Hero()
 hero3 = Hero()
 
 
-hero1.name"Sniper"
+hero1.name"Sniper" #tambahkan =
 hero1.health=100
 
-hero2.name"Sven"
+hero2.name"Sven" #tambahkan =
 hero2.health=200
 
-hero3.name"Ucup"
+hero3.name"Ucup" #tambahkan =
 hero3.health=1000
 
 print(hero1)
-print(hero1._dict_)
+print(hero1._dict_) # __dict__
 print(hero1.name)
