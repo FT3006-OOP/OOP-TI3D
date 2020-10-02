83dae90f3b3fabfb35fc05cf945bfff2193e5e32
yysofiyan <yanyansofiyan89@outlook.com>
2020-10-02 19:40:34 +0700

    dell

--- A2.1900116/Main.py
+++ A2.1900116/Main.py
@@ -2,11 +2,11 @@ class hero: # template
     pass
 
 
-hero1 = Hero() # object / instance (instansiate)
-hero2 = Hero()
-hero3 = Hero();
+hero1 = hero() # object / instance (instansiate)
+hero2 = hero()
+hero3 = hero()
 
-hero1.namem = "sniper"
+hero1.name = "sniper"
 hero1.health = 100
 
 hero2.name = "sven"
@@ -16,5 +16,5 @@ hero3.name = "ucup"
 hero3.health = 1000
 
 print(hero1)
-print(hero1._dict_)
+print(hero1.__dict__)
 print(hero1.name)
