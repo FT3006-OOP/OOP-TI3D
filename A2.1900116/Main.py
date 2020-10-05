bc6cef0b15227768d2efa0001f56d2432d4da37c
MuhamadRidwanAsidiq <a2.19001162@mhs.stmik-sumedang.ac.id>
2020-10-05 16:40:21 +0700

    menambahkan File Main.py

--- A2.1900116/Main.py
+++ A2.1900116/Main.py
@@ -1,10 +1,10 @@
-class hero: # template
+class Hero: # template
     pass
 
 
 hero1 = Hero() # object / instance (instansiate)
 hero2 = Hero()
-hero3 = Hero();
+hero3 = Hero()
 
 hero1.namem = "sniper"
 hero1.health = 100
@@ -16,5 +16,5 @@ hero3.name = "ucup"
 hero3.health = 1000
 
 print(hero1)
-print(hero1._dict_)
+print(hero1.__dict__)
 print(hero1.name)
