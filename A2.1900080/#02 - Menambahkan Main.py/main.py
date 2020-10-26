a55488d948caeb9ba595cda29fcc1bcb41314431
heru-khaerul-cahyana <a2.1900080@mhs.stmik-sumedang.ac.id>
2020-10-26 12:40:54 +0700

    Latihan OOP Game Basic

--- A2.1900080/#02 - Menambahkan Main.py/main.py
+++ A2.1900080/#02 - Menambahkan Main.py/main.py
@@ -1,10 +1,9 @@
 class Hero:
     pass
 
-
-hero1 = Hero(); # object /instance (instantiate)
+hero1 = Hero() # object /instance (instantiate)
 hero2 = Hero()
-hero3 = Hero();
+hero3 = Hero()
 
 hero1.name = "sniper"
 hero1.health = 100
