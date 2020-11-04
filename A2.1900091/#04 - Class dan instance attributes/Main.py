da058e8efe08a15a9660ad24014c5656383a2f02
Rhadika <a2.1900091@mhs.stmik-sumedang.ac.id>
2020-11-04 23:55:31 +0700

    Membuat #03-Constructor_init_()

---
+++ A2.1900091/#04 - Class dan instance attributes/Main.py
@@ -0,0 +1,20 @@
+class Hero: #template
+    #class variabel
+    jumlah = 0
+
+    def _init_(self, inputName, inputHealth, inputPower, inputArmor):
+        # onstance variabel
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
+        Hero.jumlah += 1
+        print("membuat Hero dengan nama" + inputName)
+
+
+hero1 = Hero("sniper", 100, 10, 4)
+print(Hero.jumlah)
+hero2 = Hero("mirana", 100, 15,1)
+print(Hero.jumlah)
+Hero3 = Hero("ucup", 1000, 100, 0)
+
