39729b7a71dbcf1e1eecb653050c779e2295ec8c
A21900037 <a2.1900037@mhs.stmik-sumedang.ac.id>
2020-11-05 13:23:12 +0700

    direktori

--- A2.1900037/Constructor - #3/main.py
+++ A2.1900037/Constructor - #3/main.py
@@ -1,4 +1,4 @@
-class Hero: # template
+class Hero: # template 
 
     def __init__(self, inputName, inputHealth, inputPower, inputArmor):
         self.name = inputName
@@ -7,9 +7,9 @@ class Hero: # template
         self.armor = inputArmor
 
 
-hero1 = Hero("sniper", 100, 10, 4)
-hero2 = Hero("mirana", 100, 15, 1)
-hero3 = Hero("ucup", 1000, 100, 0)
+hero1 = Hero("sniper",100, 10, 4)
+hero2 = Hero("mirana",100, 15, 1)
+hero3 = Hero("ucup",1000, 100, 0)
 
 print(hero1.__dict__)
 print(hero2.__dict__)
