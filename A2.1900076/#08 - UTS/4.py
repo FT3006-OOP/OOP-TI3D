b48ac33f947231a66994092956356f80d0cd9523
A21900076 <a2.1900076@mhs.stmik-sumedang.ac.id>
2020-11-09 15:04:33 +0700

    Menjawab Tugas UTS

---
+++ A2.1900076/#08 - UTS/4.py
@@ -0,0 +1,11 @@
+nama_bahasa = "phyton"
+
+P3MBU4T = "Guide van Rossum"
+
+Tahun_dibuatnya = 1991
+
+contoh_perusahaan_yang_menggunakan_bahasa_phyton = "Youtubr, Path, dan lain-lain."
+
+belajar_phyton_oop = True
+
+em4ang_s3ru = "Benar"
