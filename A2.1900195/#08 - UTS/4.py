25ba3c87dae2c0ab8a3cf62cbe4136a5cf155bf7
Romadhoni07 <a2.1900195@mhs.stmik-sumedang.ac.id>
2020-11-15 18:06:10 +0700

    UTS

---
+++ A2.1900195/#08 - UTS/4.py
@@ -0,0 +1,13 @@
+nama_bahasa = "python"
+
+PEMBUAT = "Guido Van Rossum"
+
+Tahun_dibuatnya = 1991
+
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
+
+belajar_python_oop = "True"
+
+Emang_seru = "Benar"
+
+print(nama_bahasa, PEMBUAT, Tahun_dibuatnya, contoh_perusahaan_yang_menggunakan_bahasa_python, belajar_python_oop, Emang_seru)
