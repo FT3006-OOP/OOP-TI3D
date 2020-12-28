f66762e4c254f037092bd584f1e1442abd3ba32e
AnggiApriyanti <Anggiap1927@gmail.com>
2020-12-29 00:13:10 +0700

    membuat direktori A2.1900018

--- A2.1900018/pratikum 2/Main.py
+++ A2.1900018/pratikum 2/Main.py
@@ -1,21 +1,19 @@
-class Hero : #template
-    pass
+class Hero:
+     pass
 
-
-hero1 = Hero()#object / instance
+hero1 = Hero()
 hero2 = Hero()
 hero3 = Hero()
 
+hero1.name = "Sniper"
+hero1.health = 100
 
-hero1.name"Sniper"
-hero1.health=100
+hero2.name = "Sven"
+hero2.health = 200
 
-hero2.name"Sven"
-hero2.health=200
-
-hero3.name"Ucup"
-hero3.health=1000
+hero3.name = "ucup"
+hero3.health = 1000
 
 print(hero1)
-print(hero1._dict_)
+print(hero1.__dict__)
 print(hero1.name)
